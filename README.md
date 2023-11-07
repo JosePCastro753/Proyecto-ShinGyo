13967d2b48eaa3161c3563ae945ef6b91bd24db7
JosePCastro753 <148279146+JosePCastro753@users.noreply.github.com>
2023-11-06 21:14:21 -0600

    Uploading local files

---
+++ README.md
@@ -0,0 +1,18 @@
+# Shin DMS
+`./Shin_DMS/` es un proyecto que busca desarrollar un sitema multi-agente capas de tomar deciciones sobre una simulacion realista de una linea de produccion, implicando todas las situaciones imprevistas y aleatorias presentes en una, sean productos defectuosos, paros de maquina no pronosticada, reprocesos, entre otros.<br>
+Tambien busca dotar al sistema de la cualidad humana del "apetito de riesgo", entrenando a los modelos del sistema para que asuman mas o menos riesgos a la hora de tomar decisiones sobre la linea de produccion. Esto es una caracteristica relevante para las industrias ya que, dependiendo del contexto en que la empresa, se puede preferir ser mas reserado o arriesgado en el manejo de la produccion.
+
+## Estado Actual 
+Actualmente `./Shin_DMS/` ha demostrado que el proyecto es factible de realizarse y es en si mismo un sistema funcional, mas no uno confiable, ni optimo.<br>
+actualmente el proyecto se puede considerar en una etapa de desarrollo con un norte definido y una base sobre la cual mejorar.
+#### Grafica de aprendizaje de Shin DMS
+<img src="/Shin_DMS/Resultados_1/grafica_aprendizaje.jpeg" height="250px" width="400px" ><br>
+Datos tomados de una corrida de aprendizaje de 1000 episodios.<br>
+Muestra el puntaje obtenido por cada episodio.
+
+## Cambios Planeados
+`./Shin^2_DMS/` [Shin Shin DMS] toma como base a su antecesor, y busca robustecer sus algoritmos de aprendizaje tanto a nivel de codigo como estadistico, balancear su ecosistema, sofisticar y personalizar tanto sus agentes como los modeles relacionados a estos y corregir errores de su antecesor.<br>
+Actualmente existen los siguiente cambios planeados:
+- Balancear los porcentajes de piezas defectuosas y reprocesos en la simulacion, asi como las recompensas asociados a los resultados de la misma. Esto dado que se noto que los valores actuales no coinciden aportan correctamente a orientar al sistema en la direccion deseada.
+- Transformar el ciclo de ejecucion de episodios actual, a un ciclo donde en cada episodio se generar un ecosistema aleatorio y se realizaran multiples iteraciones sobre un este antes de pasar al siguiente episodio. Esto con el fin de enseñar al sistemaa resolver una situacion antes de cambiarla, esto dado que entre ecosistemas, la forma de abordarlos y las soluciones optimas varian considerablemente.
+- Reservar una mayor cantidad de recursos para ejecutar multiples corridas de aprendizaje a modo de prueba o exploracion, con mayor cantidad de episodios por corrida e iteraciones por episodio.
---
+++ Shin_DMS/main.py
@@ -0,0 +1,139 @@
+import numpy as np
+import os
+import argparse
+import pandas as pd
+import Enviroment as envr
+from dqn_agent import Agent
+
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+class Environment(object):
+
+    def __init__(self, arguments):
+        self.env = envr.Scenario()
+        self.episodes_number = arguments['episode_number']
+        self.render = arguments['render']
+        self.test = arguments['test']
+
+    def run(self, agent_completar_lotes, agent_meter_urgentes_1 , agent_meter_urgentes_2, agent_establecer_buffer, agent_establecer_drum, rewards_file):
+        max_score = -10000
+        state = self.env.reset()
+        state = np.array(state)
+        state = state.ravel()
+        done = True
+        reward_all = 0
+
+
+        for episode_num in range(self.episodes_number):
+            actions = []
+
+            action_completar_lotes = agent_completar_lotes.greedy_actor(state)
+            action_meter_urgentes_1 = agent_meter_urgentes_1.greedy_actor(state)
+            action_meter_urgentes_2 = agent_meter_urgentes_2.greedy_actor(state)
+            action_establecer_buffer = agent_establecer_buffer.greedy_actor(state)
+            action_establecer_drum = agent_establecer_drum.greedy_actor(state)
+
+            actions.append(action_completar_lotes)
+            actions.append(action_meter_urgentes_1)
+            actions.append(action_meter_urgentes_2)
+            actions.append(action_establecer_buffer)
+            actions.append(action_establecer_drum)
+
+            reward = self.env.step(action_completar_lotes, action_meter_urgentes_1, action_meter_urgentes_2, action_establecer_buffer, action_establecer_drum)
+
+
+            next_state = self.env.reset()
+            next_state = np.array(next_state)
+            next_state = next_state.ravel()
+
+            #if not self.test:
+            x = True
+            if x == True:
+                agent_completar_lotes.observe((state,actions, reward, next_state, done))
+                agent_meter_urgentes_1.observe((state,actions, reward, next_state, done))
+                agent_meter_urgentes_2.observe((state,actions, reward, next_state, done))
+                agent_establecer_buffer.observe((state,actions, reward, next_state, done))
+                agent_establecer_drum.observe((state,actions, reward, next_state, done))
+
+                agent_completar_lotes.decay_epsilon()
+                agent_meter_urgentes_1.decay_epsilon()
+                agent_meter_urgentes_2.decay_epsilon()
+                agent_establecer_buffer.decay_epsilon()
+                agent_establecer_drum.decay_epsilon()
+
+                agent_completar_lotes.replay()
+                agent_meter_urgentes_1.replay()
+                agent_meter_urgentes_2.replay()
+                agent_establecer_buffer.replay()
+                agent_establecer_drum.replay()
+
+                agent_completar_lotes.update_target_model()
+                agent_meter_urgentes_1.update_target_model()
+                agent_meter_urgentes_2.update_target_model()
+                agent_establecer_buffer.update_target_model()
+                agent_establecer_drum.update_target_model()
+
+            state = next_state
+            reward_all += reward
+            self.resumen = self.env.info()
+            list = [{'time': episode_num, 'score': reward, 'escenario': self.resumen[0], 'completar_lotes': action_completar_lotes,
+                     'meter_urgentes_1': action_meter_urgentes_1, 'meter_urgentes_2': action_meter_urgentes_2,
+                     'establecer_buffer': action_establecer_buffer, 'establecer_drum': action_establecer_drum,
+                     'resultados': self.resumen[2]}]
+            if self.render:
+                print(self.resumen)
+            df = pd.DataFrame(list)
+            df.to_csv(rewards_file, mode='a', header=False, index=False)
+            if not self.test:
+                if episode_num % 100 == 0:
+                    if reward_all > max_score:
+                        agent_completar_lotes.brain.save_model()
+                        agent_meter_urgentes_1.brain.save_model()
+                        agent_meter_urgentes_2.brain.save_model()
+                        agent_establecer_buffer.brain.save_model()
+                        agent_establecer_drum.brain.save_model()
+                        max_score = reward_all
+
+
+
+seguro = False
+if seguro == 569895:
+
+    parser = argparse.ArgumentParser()
+    # Parametros DQN
+    parser.add_argument('-e', '--episode-number', default=1000000, type=int, help='Number of episodes')
+    parser.add_argument('-l', '--learning-rate', default=0.00005, type=float, help='Learning rate')
+    parser.add_argument('-op', '--optimizer', choices=['Adam', 'RMSProp'], default='RMSProp',
+                        help='Optimization method')
+    parser.add_argument('-m', '--memory-capacity', default=1000000, type=int, help='Memory capacity')
+    parser.add_argument('-b', '--batch-size', default=64, type=int, help='Batch size')
+    parser.add_argument('-ps', '--prioritization-scale', default=0.5, type=float, help='Scale for prioritization')
+    parser.add_argument('-t', '--target-frequency', default=10000, type=int,
+                        help='Number of steps between the updates of target network')
+    parser.add_argument('-x', '--maximum-exploration', default=100000, type=int, help='Maximum exploration step')
+    parser.add_argument('-rs', '--replay-steps', default=4, type=float, help='Steps between updating the network')
+    parser.add_argument('-nn', '--number-nodes', default=256, type=int, help='Number of nodes in each layer of NN')
+    parser.add_argument('-tt', '--target-type', choices=['DQN', 'DDQN'], default='DDQN')
+    parser.add_argument('-mt', '--memory', choices=['UER', 'PER'], default='PER')
+    parser.add_argument('-pl', '--pmax-random-movesrioritization-scale', default=0.5, type=float, help='Scale for prioritization')
+    parser.add_argument('-du', '--dueling', action='store_true', help='Enable Dueling architecture if "store_false" ')
+
+    parser.add_argument('-gn', '--gpu-num', default='3', type=str, help='Number of GPU to use')
+    parser.add_argument('-test', '--test', action='store_true', help='Enable the test phase if "store_false"')
+
+    # Parametros simulacion
+    parser.add_argument('-r', '--render', action='store_false', help='Activa la vizualizacion si "store_false"')
+    args = vars(parser.parse_args())
+    os.environ['CUDA_VISIBLE_DEVICES'] = args['gpu_num']
+    state_size = 9
+    action_space_binary = 2
+    action_space_box = 10001
+    rewards_file = "record.csv"
+    env = Environment(args)
+
+    agent_completar_lotes = Agent(state_size, action_space_binary, 0, 'b1.h5', args)
+    agent_meter_urgentes_1 = Agent(state_size, action_space_binary, 1, 'b2.h5', args)
+    agent_meter_urgentes_2 = Agent(state_size, action_space_binary, 2, 'b3.h5', args)
+    agent_establecer_buffer = Agent(state_size, action_space_box, 3, 'b4.h5', args)
+    agent_establecer_drum = Agent(state_size, action_space_box, 4, 'b5.h5', args)
+
+    env.run(agent_completar_lotes, agent_meter_urgentes_1 , agent_meter_urgentes_2, agent_establecer_buffer, agent_establecer_drum, rewards_file)
